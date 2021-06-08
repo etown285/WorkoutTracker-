10369c59642db7aaf498247370afdf7cb2d1150e
etown285 <78112738+etown285@users.noreply.github.com>
2021-06-07 21:28:21 -0400

    added put requests

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const db = require("../models"); 
 
     module.exports = function(app) {
     // App.get to retrieve data for the workout
@@ -19,3 +20,22 @@ const router = require("express").Router();
       });
     });
 };
+
+   // App.post to submit newly completed workouts
+   app.post("/api/workouts/", (req, res) => {
+    db.Workout.create(req.body).then((dbWorkout) => {
+      res.json(dbWorkout);
+    }).catch(err => {
+        res.status(400).json(err);
+      });
+  });
+  // App.put to update the workouts by MongoDB _id value as well as update the exercise body
+  app.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findByIdAndUpdate(
+      { _id: req.params.id }, { exercises: req.body }
+    ).then((dbWorkout) => {
+      res.json(dbWorkout);
+    }).catch(err => {
+      res.status(400).json(err);
+    });
+});
