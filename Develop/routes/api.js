6d8679a3f57b6420c94e11bffc8f22a9aeadbfb2
etown285 <78112738+etown285@users.noreply.github.com>
2021-06-07 22:28:55 -0400

    added additional seeds and fixed route error

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -1,7 +1,6 @@
-const router = require("express").Router();
-const db = require("../models"); 
+const db = require("../models");
 
-    module.exports = function(app) {
+module.exports = function(app) {
     // App.get to retrieve data for the workout
     app.get("/api/workouts", (req, res) => {
         db.Workout.find({}).then(dbWorkout => {
@@ -19,23 +18,22 @@ const db = require("../models");
         res.status(400).json(err);
       });
     });
-};
-
-   // App.post to submit newly completed workouts
-   app.post("/api/workouts/", (req, res) => {
-    db.Workout.create(req.body).then((dbWorkout) => {
-      res.json(dbWorkout);
-    }).catch(err => {
-        res.status(400).json(err);
+    // App.post to submit newly completed workouts
+    app.post("/api/workouts/", (req, res) => {
+        db.Workout.create(req.body).then((dbWorkout) => {
+          res.json(dbWorkout);
+        }).catch(err => {
+            res.status(400).json(err);
+          });
       });
-  });
-  // App.put to update the workouts by MongoDB _id value as well as update the exercise body
-  app.put("/api/workouts/:id", (req, res) => {
-    db.Workout.findByIdAndUpdate(
-      { _id: req.params.id }, { exercises: req.body }
-    ).then((dbWorkout) => {
-      res.json(dbWorkout);
-    }).catch(err => {
-      res.status(400).json(err);
+      // App.put to update the workouts by MongoDB _id value as well as update the exercise body
+      app.put("/api/workouts/:id", (req, res) => {
+        db.Workout.findByIdAndUpdate(
+          { _id: req.params.id }, { exercises: req.body }
+        ).then((dbWorkout) => {
+          res.json(dbWorkout);
+        }).catch(err => {
+          res.status(400).json(err);
+        });
     });
-});
+};
--- Develop/server.js
+++ Develop/server.js
@@ -2,23 +2,23 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000;
-
-const User = require("./userModel.js");
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(logger("dev"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workouttracker-", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workouttracker-", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
-app.use(require("./routes/api.js"));
+require("./routes/htmlroutes")(app);
+require("./routes/api")(app);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
-})
+});
